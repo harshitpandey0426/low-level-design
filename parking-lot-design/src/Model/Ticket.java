6d064d80d53415cafbf2a7066988a584f4274373
harshitpandey0426 <harshitpandey0426@gmail.com>
2023-07-31 00:00:41 +0530

    added ticket in parking lot

--- parking-lot-design/src/Model/Ticket.java
+++ parking-lot-design/src/Model/Ticket.java
@@ -1,2 +1,65 @@
-package Model;public class Ticket {
+package Model;
+
+import service.ParkingSlot;
+//redundant not used, but in interview create seperate vehicle class
+public class Ticket {
+    String ticketNumber;
+    long startTime;
+    long endTime;
+    Vehicle vehicle;
+    ParkingSlot parkingSlot;
+
+    public static Ticket createTicket(Vehicle vehicle, ParkingSlot parkingSlot){
+        long startTime = System.currentTimeMillis();
+        String ticketNumber = vehicle.getVehicleNumber() + startTime;
+
+        Ticket ticket = new Ticket();
+        ticket.setParkingSlot(parkingSlot);
+        ticket.setStartTime(startTime);
+        ticket.setVehicle(vehicle);
+        ticket.setTicketNumber(ticketNumber);
+
+        return ticket;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public ParkingSlot getParkingSlot() {
+        return parkingSlot;
+    }
+
+    public void setParkingSlot(ParkingSlot parkingSlot) {
+        this.parkingSlot = parkingSlot;
+    }
 }
+
